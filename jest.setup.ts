b2e7b687a9ee947120ea39a698bae8195804fd20
Jack Wuerfel <jwuerfel23@gmail.com>
2024-11-21 08:03:54 -0500

    remove unneeded code

--- jest.setup.ts
+++ jest.setup.ts
@@ -5,24 +5,3 @@ import 'cross-fetch/polyfill'
 const { TextEncoder, TextDecoder } = require('util')
 global.TextEncoder = TextEncoder
 global.TextDecoder = TextDecoder
-
-
-global.IntersectionObserver = class IntersectionObserver {
-    constructor() {}
-  
-    disconnect() {
-      return null;
-    }
-  
-    observe() {
-      return null;
-    }
-  
-    takeRecords() {
-      return null;
-    }
-  
-    unobserve() {
-      return null;
-    }
-  };
